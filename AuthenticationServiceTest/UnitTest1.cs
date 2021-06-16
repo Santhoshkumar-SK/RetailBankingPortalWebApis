6d325354bb8ea575fc3b330271555db96b5923fa
Yasini2001 <17tucs258@skct.edu.in>
2021-06-16 11:54:32 +0530

    Authentication Testing Completed

--- AuthenticationServiceTest/UnitTest1.cs
+++ AuthenticationServiceTest/UnitTest1.cs
@@ -1,3 +1,10 @@
+using AuthenticationService;
+using AuthenticationService.Controllers;
+using AuthenticationService.Models;
+using AuthenticationService.Repository;
+using AuthenticationService.Data;
+using Microsoft.Extensions.Configuration;
+using Moq;
 using NUnit.Framework;
 
 namespace AuthenticationServiceTest
@@ -10,9 +17,30 @@ namespace AuthenticationServiceTest
         }
 
         [Test]
-        public void Test1()
+        public void IsTokenNotNullIsTokenNotNull_When_ValidUserCredentialsAreUsed_Employee()
         {
-            Assert.Pass();
+            Mock<IAuthenticationRepo> config = new Mock<IAuthenticationRepo>();
+           AuthenticationController TokenObj = new AuthenticationController((IAuthenticationRepo)config.Object);
+            var Result = TokenObj.login(new  UserDTO{ UserId = 1, Password = "1234", Role = "Employee" });
+            Assert.IsNotNull(Result);
+        }
+
+        [Test]
+        public void IsTokenNotNullIsTokenNotNull_When_ValidUserCredentialsAreUsed_Customer()
+        {
+            Mock<IAuthenticationRepo> config = new Mock<IAuthenticationRepo>();
+            AuthenticationController TokenObj = new AuthenticationController((IAuthenticationRepo)config.Object);
+            var Result = TokenObj.login(new UserDTO { UserId = 1, Password = "1234", Role = "Customer" });
+            Assert.IsNotNull(Result);
+        }
+
+        [Test]
+        public void IsTokenNull_When_InvalidUserCredentialsAreUsed()
+        {
+            Mock<IAuthenticationRepo> config = new Mock<IAuthenticationRepo>();
+            var TokenObj = new AuthenticationController((IAuthenticationRepo)config.Object);
+            var Result = TokenObj.login(new UserDTO() { UserId = 0, Password = "wronginput", Role = "wronginput" });
+            Assert.IsNull(Result);
         }
     }
 }
