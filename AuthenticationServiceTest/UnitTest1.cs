c8d70c564fa0a5da13e6a297d55c64709214de93
Yasini2001 <17tucs258@skct.edu.in>
2021-06-21 08:37:12 +0530

    Authentication Test Part Updated

--- AuthenticationServiceTest/UnitTest1.cs
+++ AuthenticationServiceTest/UnitTest1.cs
@@ -6,41 +6,151 @@ using AuthenticationService.Data;
 using Microsoft.Extensions.Configuration;
 using Moq;
 using NUnit.Framework;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AuthenticationServiceTest
 {
     public class Tests
     {
+        private Mock<IAuthenticationRepo> _config;
+        private AuthenticationController _TokenObj;
+
         [SetUp]
         public void Setup()
         {
+            _config = new Mock<IAuthenticationRepo>();
+            _TokenObj = new AuthenticationController(_config.Object);
         }
 
         [Test]
         public void IsTokenNotNullIsTokenNotNull_When_ValidUserCredentialsAreUsed_Employee()
         {
-            Mock<IAuthenticationRepo> config = new Mock<IAuthenticationRepo>();
-           AuthenticationController TokenObj = new AuthenticationController((IAuthenticationRepo)config.Object);
-            var Result = TokenObj.login(new  UserDTO{ UserId = 1, Password = "1234", Role = "Employee" });
+            //Arrange
+            var Result = _TokenObj.login(new  UserDTO{ UserId = 1, Password = "pas@123", Role = "Employee" });
+            
+            //Assert
             Assert.IsNotNull(Result);
         }
 
         [Test]
         public void IsTokenNotNullIsTokenNotNull_When_ValidUserCredentialsAreUsed_Customer()
         {
-            Mock<IAuthenticationRepo> config = new Mock<IAuthenticationRepo>();
-            AuthenticationController TokenObj = new AuthenticationController((IAuthenticationRepo)config.Object);
-            var Result = TokenObj.login(new UserDTO { UserId = 1, Password = "1234", Role = "Customer" });
+            //Arrange
+            var Result = _TokenObj.login(new UserDTO { UserId = 1, Password = "pas@123", Role = "Customer" });
+            
+            //Assert
             Assert.IsNotNull(Result);
         }
 
         [Test]
         public void IsTokenNull_When_InvalidUserCredentialsAreUsed()
         {
-            Mock<IAuthenticationRepo> config = new Mock<IAuthenticationRepo>();
-            var TokenObj = new AuthenticationController((IAuthenticationRepo)config.Object);
-            var Result = TokenObj.login(new UserDTO() { UserId = 0, Password = "wronginput", Role = "wronginput" });
+            //Arrange
+            var Result = _TokenObj.login(new UserDTO() { UserId = 0, Password = "wronginput", Role = "wronginput" });
+            
+            //Assert
             Assert.IsNull(Result);
         }
+
+        [Test]
+        public void IsTokenNotNull_When_CreateCustomer_Valid_Details()
+        {
+            //Arrange
+            var Result = _TokenObj.addCustomer(new UserDTO { UserId = 1, Password = "pas@123", Role = "Customer" });
+            
+            //Assert
+            Assert.IsNotNull(Result);
+        }
+
+        [Test]
+        public void IsTokenNull_When_CreateCustomer_InValid_Details()
+        {
+            //Arrange
+            var Result = _TokenObj.addCustomer(new UserDTO { UserId = 0, Password = "Wronginput", Role = "wronginput" });
+            
+            //Assert
+            Assert.IsNull(Result);
+        }
+
+        [Test]
+        public void IsTokenNotNull_When_GenerateToken_Valid_ReturnsOk()
+        {
+           var Result = _config.Setup(s => s.generateJSONWebToken(It.IsAny<UserDTO>())).Returns("True");
+           Assert.IsNotNull(Result);
+        }
+
+        [Test]
+        public void IsTokenNull_When_GenerateToken_InValid_ReturnsOk()
+        {
+            var Result = _config.Setup(s => s.generateJSONWebToken(It.IsAny<UserDTO>())).Returns("False");
+            Assert.IsNull(Result);
+        }
+
+        [Test]
+        public void IsTokenNotNull_When_AuthenticateEmployee_Valid_ReturnOk()
+        {
+            var Result = _config.Setup(s => s.authenticateEmployee(new UserDTO
+            {
+                UserId = 1,
+                Password = "pas@123",
+                Role = "Employee"
+            }));
+
+            Assert.IsNotNull(Result);
+        }
+        
+        [Test]
+        public void IsTokenNotNull_When_AuthenticateCustomer_Valid_ReturnOk()
+        {
+            var Result = _config.Setup(s => s.authenticateCustomer(new UserDTO
+            {
+                UserId = 1,
+                Password = "pas@123",
+                Role = "Customer"
+            }));
+
+            Assert.IsNotNull(Result);
+        }
+
+        [Test]
+        public void IsTokenNull_When_AuthenticateUser_InValid_ReturnOk()
+        {
+            var Result = _config.Setup(s => s.authenticateUser(new UserDTO
+            {
+                UserId = 1,
+                Password = "wronginput",
+                Role = "wronginput"
+            }));
+
+            Assert.IsNull(Result);
+        }
+
+        [Test]
+        public void IsTokenNotNull_When_addCustomer_Valid_ReturnOk()
+        {
+            var Result = _config.Setup(s => s.addCustomer(new UserDTO
+            {
+                UserId = 1,
+                Password = "pas@123",
+                Role = "Customer"
+            }));
+
+            Assert.IsNotNull(Result);
+        }
+
+        [Test]
+        public void IsTokenNotNull_When_addCustomer_InValid_ReturnOk()
+        {
+            var Result = _config.Setup(s => s.addCustomer(new UserDTO
+            {
+                UserId = 1,
+                Password = "wronginput",
+                Role = "wronginput"
+            }));
+
+            Assert.IsNull(Result);
+        }
+
     }
 }
